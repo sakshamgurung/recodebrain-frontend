913874006811019de443bd6edb6f8aefebdf34aa
Saksham Gurung <laxmikgurung@gmail.com>
2022-03-24 06:56:18 +0545

    Adds mobile menu and newsletter section.

--- components/Layout/MainNavigation.js
+++ components/Layout/MainNavigation.js
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 
 import Link from "next/link";
+import { FaHamburger } from "react-icons/fa";
+import FocusLock from "react-focus-lock";
 
 import classes from "./MainNavigation.module.css";
+import MobileMenu from "./MobileMenu";
 
 function MainNavigation() {
 	const [show, setShow] = useState(true);
 	const [lastScrollY, setLastScrollY] = useState(0);
+	const [openMobileMenu, setOpenMobileMenu] = useState(false);
 
 	const controlNavbar = () => {
 		if (typeof window !== "undefined") {
@@ -28,12 +32,22 @@ function MainNavigation() {
 		};
 	}, [lastScrollY]);
 
+	const toggleMobileMenu = () => {
+		setOpenMobileMenu(!openMobileMenu);
+	};
+
 	return (
 		<header className={`${classes.header}`}>
 			{/* <header className={`${classes.header} ${!show && classes.headerHidden}`}> */}
 			<Link href="/">
 				<a className={classes.logo}>RecodeBrain</a>
 			</Link>
+			<div className={classes.mobileMenu}>
+				<FocusLock disabled={!openMobileMenu}>
+					<FaHamburger size="1.5em" onClick={toggleMobileMenu} />
+					<MobileMenu isOpen={openMobileMenu} onClose={toggleMobileMenu} />
+				</FocusLock>
+			</div>
 			<nav className={classes.nav}>
 				<ul>
 					<li>
---
+++ components/Layout/MobileMenu.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+import classes from "./MobileMenu.module.css";
+import { AiOutlineClose } from "react-icons/ai";
+
+function MobileMenu({ isOpen, onClose }) {
+	if (!isOpen) {
+		return <></>;
+	}
+	return (
+		<nav className={classes.nav}>
+			<div className={classes.header}>
+				<AiOutlineClose size="1.5em" color="white" onClick={onClose} />
+			</div>
+			<ul>
+				<li>
+					<Link href="/about">
+						<a onClick={onClose}>About</a>
+					</Link>
+				</li>
+				<li>
+					<Link href="/series">
+						<a onClick={onClose}>Series</a>
+					</Link>
+				</li>
+				<li>
+					<Link href="/topics">
+						<a onClick={onClose}>Topics</a>
+					</Link>
+				</li>
+			</ul>
+		</nav>
+	);
+}
+
+export default MobileMenu;
