2c735de2a5719d90584374d1201dd6e57795a85f
Saksham Gurung <laxmikgurung@gmail.com>
2022-05-22 10:57:25 +0545

    Adds google analytics code

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import Head from "next/head";
+
 import Layout from "../components/layout/Layout";
 import "../styles/globals.css";
+import useGtag from "../lib/useGtag";
 
 function MyApp({ Component, pageProps }) {
+	useGtag();
+
 	return (
 		<Layout>
 			<Head>
