48c2f6f0119a338eb8337055bf8f2e3786730472
Saksham Gurung <laxmikgurung@gmail.com>
2022-09-20 12:44:40 +0545

    favicon added

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,14 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
-				<meta name="author" content="Sakchhyam" />
+				<link rel="icon" href="/icons/logo/favicon-16x16.png" sizes="16x16" type="image/png" />
+				<link rel="icon" href="/icons/logo/favicon-32x32.png" sizes="32x32" type="image/png" />
+				<link
+					rel="apple-touch-icon"
+					href="/icons/logo/apple-touch-icon.png"
+					sizes="180x180"
+					type="image/png"
+				/>
 			</Head>
 			<body className="text-gray-700 bg-light dark:bg-dark dark:text-slate-100">
 				{/* <!-- Google Tag Manager (noscript) --> */}
